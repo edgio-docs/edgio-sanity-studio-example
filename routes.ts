3f43cce1b0477d44443a6549d225bb8010ceeb18
Rishi Raj Jain <rjain@llnw.com>
2023-01-05 22:44:25 +0530

    Update to latest Edgio version

--- routes.ts
+++ routes.ts
@@ -1,9 +1,7 @@
-// This file was automatically added by edgio deploy.
+// This file was automatically added by edgio init.
 // You should commit this file to source control.
+
 import { Router } from '@edgio/core'
 import { sanityRoutes } from '@edgio/sanity-studio'
 
-export default new Router()
-  // Prevent search engines from indexing permalink URLs
-  .noIndexPermalink()
-  .use(sanityRoutes)
+export default new Router({ indexPermalink: false }).use(sanityRoutes)
